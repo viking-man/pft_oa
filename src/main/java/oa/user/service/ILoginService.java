8e05368b9737e10c63f033ddc600f195984fcfba
viking <jiangwei4953267@163.com>
2017-09-08 20:15:22 +0800

    "回家继续"

---
+++ src/main/java/oa/user/service/ILoginService.java
@@ -0,0 +1,11 @@
+package oa.user.service;
+
+/**
+ * Project : pft_oa
+ *
+ * @author : jiangwei
+ * @create : 2017/9/8-18:20
+ */
+public interface ILoginService {
+    boolean checkLoginSucc();
+}
