17cdcd1a84959a723323e47a9782ca2f28cf0a4d
viking <jiangwei4953267@163.com>
2017-09-12 19:33:01 +0800

    "回家继续"

--- src/main/java/oa/user/controller/WeixinController.java
+++ src/main/java/oa/user/controller/WeixinController.java
@@ -1,12 +1,13 @@
 package oa.user.controller;
 
-import oa.user.entity.UserEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import param.GlobleConstant;
+import weixin.AesException;
+import weixin.WXBizMsgCrypt;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * Project : pft_oa
@@ -17,8 +18,24 @@ import java.util.List;
 @Controller
 public class WeixinController {
 
-    @RequestMapping("/weixin")
+    @RequestMapping("/weixin.do")
     public String queryUserInfo(HttpServletRequest request, Model model) {
+        String msg_signature = request.getParameter("msg_signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+//        String msg_encrypt = msg.getMsg_encrypt();
+        String sEchoStr;
+        try {
+            WXBizMsgCrypt wxcpt = new WXBizMsgCrypt(GlobleConstant.WEIXIN_Token, GlobleConstant.WEIXIN_EncodingAESKey, GlobleConstant.WEIXIN_CorpID);
+            sEchoStr = wxcpt.VerifyURL(msg_signature, timestamp,
+                    nonce, echostr);
+            System.out.println("verifyurl echostr: " + sEchoStr);
+        } catch (AesException e) {
+            e.printStackTrace();
+        }
+
+
         return "userinfo";
     }
 }
--- src/main/java/param/GlobleConstant.java
+++ src/main/java/param/GlobleConstant.java
@@ -6,4 +6,9 @@ package param;
 public class GlobleConstant {
     public static final String SESSION_USER_KEY = "session_user_key";
     public static final String SESSION_LOGIN_CONTEXT = "session_login_context";
+    public static final String WEIXIN_Token = "Qafz9atbkCfB";
+    public static final String WEIXIN_EncodingAESKey = "3FEvHgmFzTFtJr3zDlGihwBdnFdBfOHZe34PBhnR7vs";
+    public static final String WEIXIN_CorpID = "wx5ba8760d25c0e797";
+    public static final String WEIXIN_AgentId = "1000002";
+    public static final String WEIXIN_Secret = "EMaJHn777HPc3BwFsOWcjrKaCDZ9fXezopfsJL42PYU";
 }
